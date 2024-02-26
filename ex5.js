1d845c46da3e1b9aac6df68be715ccfa910b1fa3
Bisma786 <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:18:07 +0500

    Initial commit

---
+++ ex5.js
@@ -0,0 +1,4 @@
+// Q:5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Andrew Tate";
+var message = famous_person + "once said, \" Do the impossible and you will never doubt yourself again\"";
+console.log(message);
---
+++ ex5.ts
@@ -0,0 +1,5 @@
+// Q:5 Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person:string="Andrew Tate"
+let message:string= famous_person+`once said, " Do the impossible and you will never doubt yourself again"`
+console.log(message)
